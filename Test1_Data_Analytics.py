287aac44d12c6f7b1ab876125d6dbfb21bcb106d
Abdullah Anwar <61405213+aanwar5@users.noreply.github.com>
2020-09-01 13:33:24 +1000

    Add files via upload

---
+++ Test1_Data_Analytics.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+pwd
+
+
+# In[4]:
+
+
+print("Welcome to Python Jupyter Notebook Session ")
+
+
+# In[6]:
+
+
+print("Hello World")
+
+
+# In[9]:
+
+
+name = input("Enter your household in Victoria, Australia: \t\t")
+
+
+# In[11]:
+
+
+print("Welcome", name)
+
+
+# In[14]:
+
+
+import math 
+math.sqrt(81)
+
+
+# In[16]:
+
+
+import sys
+print(sys.version)
+
+
+# In[17]:
+
+
+import keyword
+print(keyword.kwlist)
+
