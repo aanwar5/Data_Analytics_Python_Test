61b500eb997178aeccfd83139d498dd03c79ab12
Abdullah Anwar <61405213+aanwar5@users.noreply.github.com>
2020-09-01 13:49:46 +1000

    Tables Calculator on Python
    Tables Calculator for further concept building of Data Structures!

---
+++ Tables Calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#Making a multiplication table on Python 
+j=1 
+while (j<=10):
+    i = 1
+    print("Printing the following table of ", j)
+    while(i<=10):
+        print( j, "times", i, j*i)
+        i=i+1
+    j =j+1 
+    
+        
+
+
+# In[ ]:
+
+
+
+
